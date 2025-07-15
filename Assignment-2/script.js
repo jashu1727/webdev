188b88a8c1b72001299bb42e257001b7cfc6175f
Jashu1727 <jashwanthjashu1727@gmail.com>
2025-07-15 15:37:27 +0530

    added assignment 2

---
+++ Assignment-2/script.js
@@ -0,0 +1,36 @@
+    function randomcolor() {
+        var letters = '0123456789ABCDEF';
+        var color = '#';
+
+        for (var i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+
+        return color;
+    }
+
+
+    function show() {
+        var left = Math.floor(Math.random() * 1001);
+        var top = Math.floor(Math.random() * (300 - 50 + 1)) + 50;
+        var size = Math.floor(Math.random() * (300 - 50 + 1)) + 50;
+        document.getElementById("box").style.display="block";
+        document.getElementById("box").style.backgroundColor=randomcolor();
+        document.getElementById("box").style.marginLeft=left+"px";
+        document.getElementById("box").style.marginTop=top+"px";
+        document.getElementById("box").style.width=size+"px";
+        document.getElementById("box").style.height=size+"px";
+
+        
+        start = new Date().getTime();
+    }
+    var start;
+
+    document.getElementById("box").onclick = function() {
+        document.getElementById("box").style.display="none";
+        var end = new Date().getTime();
+        var time = (end-start)/1000;
+        alert("Your reaction time: " + time + " seconds"); 
+        setTimeout(show, Math.random() * 1000 + 1000);
+        
+    };
